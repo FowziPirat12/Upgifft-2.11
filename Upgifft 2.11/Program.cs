de7eb8b06be403da05b3877ccdbdc4339b643d47
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 10:53:23 +0200

    Add project files.

---
+++ Upgifft 2.11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Upgift_2._11
+{
+    class Program
+    {
+        static void Main(String[]args)
+        {
+            Console.WriteLine("Hur långt hoppade Alma och Elin mätt i meter?");
+            string höjdText = Console.ReadLine();
+            double höjd = double.Parse(höjdText);
+            double skillnad = 1.30 - höjd;
+            Console.WriteLine("Elin hoppade" + skillnad + "meter högre än Alma");
+        }
+    }
+}
